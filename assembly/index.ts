07ae6c166b55e303c363b10309ee92cacff12fe4
Brandon <brandonjmhunt@gmail.com>
2022-05-27 00:40:13 -0600

    feat: DataCollection Prototyping, callback design

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,81 +1,51 @@
 import { parsePrices, console } from "@steerprotocol/strategy-utils";
 import { JSON } from "assemblyscript-json";
-import { CustomStrategy } from "./CustomStrategy";
+import {UniswapSwap} from "./types/Uniswap";
+import {UniswapDataCollector} from "./UniswapDataCollector";
+// import { CustomStrategy } from "./CustomStrategy";
 
-// This is the main entry point for the bundle. It is called when the bundle is loaded by the worker.
-// Strategy is always exported verbatim so that the worker can find the proper reference.
-export class Strategy {
-  // Local instance of the strategy
-  customStrategy: CustomStrategy;
 
-  // Parse the input strings into proper types and initialize the strategy.
-  constructor(config: string) {
-    // Parse config object JSON string
-    const parsedConfig = <JSON.Obj>JSON.parse(config.toString());
+// The class designed for a certain number of data sources
+export class DataConnector {
 
-    // Parse input config object
-    let percentJSON = parsedConfig.getInteger("percent");
-    let binWidthJSON = parsedConfig.getInteger("binWidth");
+  public SwapData: Array<UniswapSwap> = [];
+  public uniswapHandler: UniswapDataCollector;
 
-    // Validate the config object
-    if (percentJSON == null) {
-      throw new Error("Percent must be a provided");
-    }
-    
-    if (!binWidthJSON) {
-      throw new Error("Bin Width must be a provided");
-    }
+  // I'd imagine this input would be standardized something like this, any config and then state info necessary for getting the data
+  constructor(config: string, epochTimestamp: string) {
 
-    // Parse config values from the JSON class
-    const percent = f32(percentJSON.valueOf());
-    const binWidth = f32(binWidthJSON.valueOf());
-    
-    // Set the strategy
-    this.customStrategy = new CustomStrategy(
-      binWidth,
-      percent
-    );
+    // pull out the candle width and num candles from the config so we can calculate the dates
+    // we might have a config for holding variables for this module as well (for things like pool address or tokens)
+    // but we could also just put this in the execution config
+    width = ...
+    numCandles = ...
+    this.uniswapHandler = new UniswapDataCollector(width * numCandles, epochTimestamp);
+
   }
 
-  // After instantiation, this is called once per epoch.
-  public execute(_prices: string): string {
-    // Parse the input string into an array of Price objects
-    const prices = parsePrices(_prices);
-
-    // First let's get the trailing stop price from the customer strategy class we built
-    const trailingLimit = this.customStrategy.trailingStop(prices)
-
-    // Next we'll create the smallest possible position by providing the trailing stop price
-    // ass the upper and lower price for the liquidity position
-    const positions = this.customStrategy.getPositions(trailingLimit, trailingLimit);
-
-    // Once we have our positions we will use a helper function from the UniswapV3LiquidityStrategy
-    // to render the result which is needed for on-chain execution.
-    const result = this.customStrategy.renderResult(positions)
-
-    console.log(result);
-
-    return result;
-  }
-
-  // Renders the config object in JSON Schema format, which is used
-  // by the frontend to display input value options and validate user input.
-  static config(): string {
+  // First call from node is here to get names of functions to call, in this case only UniswapData
+  static functionCalls(): string {
     return `{
-      "$schema": "http://json-schema.org/draft-07/schema#",
-      "title": "Strategy Config",
-      "type": "object",
-      "properties": {
-        "percent": {
-            "type": "number",
-            "description": "Percent for trailing stop order"
-        },
-        "binWidth": {
-            "type": "number",
-            "description": "Width for liquidity buckets"
-        }
-      },
-      "required": ["percent", "binWidth"]
-    }`;
+      "calls" : [
+        "UniswapData"
+      ]
+    }`
   }
+
+  // We might do this part a bit differently but essentially we get what functions we need to call from the above function,
+  // which we then call and pass the result back in too see if it is ok to continue, if not we return the next request
+  // to make and that request's result gets passed in again. The boolean tells us when no more calls are necessary from this source, the string is the next request to make.
+  public UniswapData(data: string | null): [boolean, string] {
+    if (data != null) {
+      // here we would parse data into an array of swaps to pass to the call
+    }
+    // go look at this function vvv
+    return this.uniswapHandler.call(data)
+  }
+
+  // I was thinking after we loop through all the calls, we can then have a function that aggregates the results to be passed to the execution bundle
+  public results(): string {
+    return this.uniswapHandler.getData();
+  }
+
 }
