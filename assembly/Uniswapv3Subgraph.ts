710cab8e6f1a5f3714033ff5ba7bf9908f9e570e
Brandon <brandonjmhunt@gmail.com>
2022-06-01 18:41:03 -0600

    fix: :bulb: Clearing up comments for compilation

--- assembly/Uniswapv3Subgraph.ts
+++ assembly/Uniswapv3Subgraph.ts
@@ -1,204 +1,204 @@
-// // import {UniswapSwap} from "./types/Uniswap";
-// import { JSON } from "assemblyscript-json";
+// import {UniswapSwap} from "./types/Uniswap";
+import { JSON } from "assemblyscript-json";
 
-// export class Uniswapv3Subgraph {
+export class Uniswapv3Subgraph {
 
-//     public epochTimestamp: i32; 
+    public epochTimestamp: i32; 
 
-//     // Defaults
-//     private page: i32 = 0;
-//     private limit: i32 = 1000;
-//     private skip: i32 = 0;
+    // Defaults
+    private page: i32 = 0;
+    private limit: i32 = 1000;
+    private skip: i32 = 0;
 
-//     public data: Array<JSON.Value> = [];
+    public data: Array<JSON.Value> = [];
 
-//     // Variables delivered from the node at epoch runtime
-//     constructor(epochTimestamp: i32) {
-//         this.epochTimestamp = epochTimestamp;
-//     }
+    // Variables delivered from the node at epoch runtime
+    constructor(epochTimestamp: i32) {
+        this.epochTimestamp = epochTimestamp;
+    }
 
-//     // GetSwaps persistant variables
-//     private GetSwaps_beginingDate: i32;
-//     // public GetSwaps_data: Array<UniswapSwap> = [];
+    // GetSwaps persistant variables
+    private GetSwaps_beginingDate: i32;
+    // public GetSwaps_data: Array<UniswapSwap> = [];
 
-//     public getUniswapSwap<T>(response: T, poolAddress: string, period: i32): [boolean, string] {
+    public getUniswapSwap<T>(response: T, poolAddress: string, period: i32): Array<string> {
 
-//         if (response == null) { // Presumably the first call, initialize the variables
-//             this.GetSwaps_beginingDate = this.epochTimestamp - period;
-//             // this.GetSwaps_endDate = this.epochTimestamp;
-//             // this.GetSwaps_poolAddress = callConfig.poolAddress;
-//             return [false, `{
-//                 "name": "Uniswapv3 Subgraph",
-//                 "method": "post",
-//                 "url": {
-//                     "enpoint": "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
-//                     "variables": {}
-//                 }
-//                 "headers": {}
-//                 "payload": {
-//                     "params": {},
-//                     "graphql": {
-//                     "query": 
-//                     "query getSwaps(address: ${poolAddress}, startDate: ${this.GetSwaps_beginingDate.toString()}, endDate: ${this.epochTimestamp.toString()}, limit: ${this.limit.toString()}, skip: 0){
-//                         swaps(first: ${this.limit.toString()}, skip: 0, where: {
-//                             timestamp_gt: ${this.GetSwaps_beginingDate.toString()}
-//                             timestamp_lt: ${this.epochTimestamp.toString()}
-//                             pool: ${poolAddress}
-//                             }, orderBy: timestamp, orderDirection: asc){
-//                             id
-//                             timestamp
-//                             amount0
-//                             amount1
-//                             transaction {
-//                                 id
-//                                 blockNumber
-//                             }
-//                             tick
-//                             sqrtPriceX96
-//                             }
-//                         }
-//                     }",
-//                     "variables": {}
-//                     }
-//                     "variables": {}
-//                 }
-//             }`
+        if (response == null) { // Presumably the first call, initialize the variables
+            this.GetSwaps_beginingDate = this.epochTimestamp - period;
+            // this.GetSwaps_endDate = this.epochTimestamp;
+            // this.GetSwaps_poolAddress = callConfig.poolAddress;
+            return ["false", `{
+                "name": "Uniswapv3 Subgraph",
+                "method": "post",
+                "url": {
+                    "enpoint": "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+                    "variables": {}
+                }
+                "headers": {}
+                "payload": {
+                    "params": {},
+                    "graphql": {
+                    "query": 
+                    "query getSwaps(address: ${poolAddress}, startDate: ${this.GetSwaps_beginingDate.toString()}, endDate: ${this.epochTimestamp.toString()}, limit: ${this.limit.toString()}, skip: 0){
+                        swaps(first: ${this.limit.toString()}, skip: 0, where: {
+                            timestamp_gt: ${this.GetSwaps_beginingDate.toString()}
+                            timestamp_lt: ${this.epochTimestamp.toString()}
+                            pool: ${poolAddress}
+                            }, orderBy: timestamp, orderDirection: asc){
+                            id
+                            timestamp
+                            amount0
+                            amount1
+                            transaction {
+                                id
+                                blockNumber
+                            }
+                            tick
+                            sqrtPriceX96
+                            }
+                        }
+                    }",
+                    "variables": {}
+                    }
+                    "variables": {}
+                }
+            }`
                 
-//             ];
-//         }
+            ];
+        }
 
-//         else {
+        else {
 
-//             // parse the response
-//             const new_response = <JSON.Obj>JSON.parse(response);
-//             const new_data = new_response.getObj("data");
-//             if (new_data == null) {throw new Error("No data in response");}
-//             const new_arr = new_data.getArr("swaps");
-//             if (new_arr == null) {throw new Error("No swaps in response");}
-//             const new_swaps = new_arr._arr;
-//             // const new_swaps = new_response.data.swaps as Array<UniswapSwap>;
+            // parse the response
+            const new_response = <JSON.Obj>JSON.parse(response);
+            const new_data = new_response.getObj("data");
+            if (new_data == null) {throw new Error("No data in response");}
+            const new_arr = new_data.getArr("swaps");
+            if (new_arr == null) {throw new Error("No swaps in response");}
+            const new_swaps = new_arr._arr;
+            // const new_swaps = new_response.data.swaps as Array<UniswapSwap>;
 
-//             // There is data being returned from a call, so we update and check if another call is needed
-//             if (new_swaps.length == 0) {
-//                 // Here if we have updated to the point where there is no more data to return,
-//                 // we return true to indicate that we are done. This means there is an extra superfluous 
-//                 // call that does nothing but confirm we are done.
-//                 return [true, ""];
-//             }
-//             else {
-//                 //Here we add our data, and update the beginingDate
-//                 this.data = this.data.concat(new_swaps);
-//                 // We add one to not include the last
-//                 const last_swap = <JSON.Obj>this.data[this.data.length - 1];
-//                 const timestamp = last_swap.getNum("timestamp");
-//                 if (timestamp == null) {throw new Error("No timestamp in last swap");}
-//                 this.GetSwaps_beginingDate = timestamp._num + 1;
-//                 return [false, `{
-//                     "name": "Uniswapv3 Subgraph",
-//                     "method": "post",
-//                     "url": {
-//                         "enpoint": "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
-//                         "variables": {}
-//                     }
-//                     "headers": {}
-//                     "payload": {
-//                         "params": {},
-//                         "graphql": {
-//                         "query": 
-//                         "query getSwaps(address: ${poolAddress}, startDate: ${this.GetSwaps_beginingDate.toString()}, endDate: ${this.epochTimestamp.toString()}, limit: ${this.limit.toString()}, skip: 0){
-//                             swaps(first: ${this.limit.toString()}, skip: 0, where: {
-//                                 timestamp_gt: ${this.GetSwaps_beginingDate.toString()}
-//                                 timestamp_lt: ${this.epochTimestamp.toString()}
-//                                 pool: ${poolAddress}
-//                                 }, orderBy: timestamp, orderDirection: asc){
-//                                 id
-//                                 timestamp
-//                                 amount0
-//                                 amount1
-//                                 transaction {
-//                                     id
-//                                     blockNumber
-//                                 }
-//                                 tick
-//                                 sqrtPriceX96
-//                                 }
-//                             }
-//                         }",
-//                         "variables": {}
-//                         }
-//                         "variables": {}
-//                     }
-//                 }`]
-//             }
-//         }
-//     }
+            // There is data being returned from a call, so we update and check if another call is needed
+            if (new_swaps.length == 0) {
+                // Here if we have updated to the point where there is no more data to return,
+                // we return true to indicate that we are done. This means there is an extra superfluous 
+                // call that does nothing but confirm we are done.
+                return ["true", ""];
+            }
+            else {
+                //Here we add our data, and update the beginingDate
+                this.data = this.data.concat(new_swaps);
+                // We add one to not include the last
+                const last_swap = <JSON.Obj>this.data[this.data.length - 1];
+                const timestamp = last_swap.getNum("timestamp");
+                if (timestamp == null) {throw new Error("No timestamp in last swap");}
+                this.GetSwaps_beginingDate = timestamp._num + 1;
+                return ["false", `{
+                    "name": "Uniswapv3 Subgraph",
+                    "method": "post",
+                    "url": {
+                        "enpoint": "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+                        "variables": {}
+                    }
+                    "headers": {}
+                    "payload": {
+                        "params": {},
+                        "graphql": {
+                        "query": 
+                        "query getSwaps(address: ${poolAddress}, startDate: ${this.GetSwaps_beginingDate.toString()}, endDate: ${this.epochTimestamp.toString()}, limit: ${this.limit.toString()}, skip: 0){
+                            swaps(first: ${this.limit.toString()}, skip: 0, where: {
+                                timestamp_gt: ${this.GetSwaps_beginingDate.toString()}
+                                timestamp_lt: ${this.epochTimestamp.toString()}
+                                pool: ${poolAddress}
+                                }, orderBy: timestamp, orderDirection: asc){
+                                id
+                                timestamp
+                                amount0
+                                amount1
+                                transaction {
+                                    id
+                                    blockNumber
+                                }
+                                tick
+                                sqrtPriceX96
+                                }
+                            }
+                        }",
+                        "variables": {}
+                        }
+                        "variables": {}
+                    }
+                }`]
+            }
+        }
+    }
 
-//     // public getGetSwapData(): Array<UniswapSwap> {
-//     //     // Wipe the data so if there is another call for swap data it will be fresh
-//     //     const temp = this.GetSwaps_data;
-//     //     this.GetSwaps_data = [];
-//     //     return temp;
-//     // }
+    // public getGetSwapData(): Array<UniswapSwap> {
+    //     // Wipe the data so if there is another call for swap data it will be fresh
+    //     const temp = this.GetSwaps_data;
+    //     this.GetSwaps_data = [];
+    //     return temp;
+    // }
 
-//     // public TransformSwapsToCandles(swaps: Array<UniswapSwap>, config: JSON): string {
-//     //     const swapData = swaps.map((d: any) => {
-//     //         return {
-//     //             index: d.timeStamp * 1000,
-//     //             value: Math.pow(1.0001,d.tick).toFixed(8),
-//     //         }
-//     //     });
-//     //     const series = timeSeries({
-//     //         name: 'swaps',
-//     //         columns: [...Object.keys(swapData[0])],
-//     //         points: swapData.map((point: any) => {
-//     //             return [...Object.values(point)].map(value => Number(value))
-//     //             }),
-//     //         })
+    // public TransformSwapsToCandles(swaps: Array<UniswapSwap>, config: JSON): string {
+    //     const swapData = swaps.map((d: any) => {
+    //         return {
+    //             index: d.timeStamp * 1000,
+    //             value: Math.pow(1.0001,d.tick).toFixed(8),
+    //         }
+    //     });
+    //     const series = timeSeries({
+    //         name: 'swaps',
+    //         columns: [...Object.keys(swapData[0])],
+    //         points: swapData.map((point: any) => {
+    //             return [...Object.values(point)].map(value => Number(value))
+    //             }),
+    //         })
         
-//     //         const windowRollup = series
-//     //         .fixedWindowRollup({
-//     //         window: window(duration(config.candleWidth * 1000)),
-//     //         aggregation: {
-//     //             high: ['value', max()],
-//     //             low: ['value', min()],
-//     //             close: ['value', last()],
-//     //             open: ['value', first()],
-//     //         },
-//     //         })
-//     //         .fill({
-//     //         fieldSpec: 'value',
-//     //         method: 2,
-//     //         })
+    //         const windowRollup = series
+    //         .fixedWindowRollup({
+    //         window: window(duration(config.candleWidth * 1000)),
+    //         aggregation: {
+    //             high: ['value', max()],
+    //             low: ['value', min()],
+    //             close: ['value', last()],
+    //             open: ['value', first()],
+    //         },
+    //         })
+    //         .fill({
+    //         fieldSpec: 'value',
+    //         method: 2,
+    //         })
         
-//     //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     //       // @ts-ignore
-//     //         const transformed = windowRollup
-//     //         .toJSON()
-//     //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     //       // @ts-ignore
-//     //         .points.map((p: any) => {
-//     //         // Convert time range to timestamp of beginning of range
-//     //         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     //         // @ts-ignore
-//     //         p[0] = new Index(p[0])
-//     //         .toTimeRange()
-//     //         .toTime(TimeAlignment.Begin)
-//     //         .toDate()
-//     //         .getTime()
-//     //         return p
-//     //         })
-//     //         .map((p: any) => {
-//     //         return {
-//     //             timestamp: p[0],
-//     //             high: p[1],
-//     //             low: p[2],
-//     //             close: p[3],
-//     //             open: p[4],
-//     //         }
-//     //         })
+    //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //       // @ts-ignore
+    //         const transformed = windowRollup
+    //         .toJSON()
+    //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //       // @ts-ignore
+    //         .points.map((p: any) => {
+    //         // Convert time range to timestamp of beginning of range
+    //         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //         // @ts-ignore
+    //         p[0] = new Index(p[0])
+    //         .toTimeRange()
+    //         .toTime(TimeAlignment.Begin)
+    //         .toDate()
+    //         .getTime()
+    //         return p
+    //         })
+    //         .map((p: any) => {
+    //         return {
+    //             timestamp: p[0],
+    //             high: p[1],
+    //             low: p[2],
+    //             close: p[3],
+    //             open: p[4],
+    //         }
+    //         })
         
-//     //     return JSON.stringify(transformed);
-//     //     }
+    //     return JSON.stringify(transformed);
+    //     }
         
-//     }
+    }
 
--- assembly/index.ts
+++ assembly/index.ts
@@ -1,26 +1,26 @@
 import { JSON } from "assemblyscript-json";
 // import { Obj } from "assemblyscript-json/assembly/JSON";
 // import {UniswapSwap} from "./types/Uniswap";
-// import {Uniswapv3Subgraph} from "./Uniswapv3Subgraph";
+import {Uniswapv3Subgraph} from "./Uniswapv3Subgraph";
 
 
 
 // The class designed for a certain number of data sources
 export class DataConnector {
 
-  // public uniswapHandler: Uniswapv3Subgraph;
+  public uniswapHandler: Uniswapv3Subgraph;
   public config: string;
   // Anything to get that MVP
-  // private supportedFunctions: Array<string> = ["UniswapSwapData"];
+  private supportedFunctions: Array<string> = ["UniswapSwapData"];
 
   // public _config: Array<Map<string,string>> = [];
 
-  public TransformedData: string;
+  // public TransformedData: string;
 
   // I'd imagine this input would be standardized something like this, any config and then state info necessary for getting the data
   constructor(config: string, epochTimestamp: string) {
 
-    // this.uniswapHandler = new Uniswapv3Subgraph(i32(parseInt(epochTimestamp)));
+    this.uniswapHandler = new Uniswapv3Subgraph(i32(parseInt(epochTimestamp)));
     this.config = config;
   }
 
@@ -29,55 +29,54 @@ export class DataConnector {
   // Either the response to the request is sent, or null
   public main<T>(response: T): Array<string> {
 
-    // //Get the function name if it exists
-    // const config = <JSON.Obj>JSON.parse(this.config);
-    // const functionToCall = config.getValue("name");
-    // // Null handling
-    // if (functionToCall == null) {
-    //   throw new Error("No function name provided");
-    // }
-    // const functionName = functionToCall.toString();
-    // // https://www.assemblyscript.org/runtime.html#memory-layout <
-    // if (this.supportedFunctions.includes(functionName) == false) {
-    //   throw new Error("Function not found");
-    // }
+    //Get the function name if it exists
+    const config = <JSON.Obj>JSON.parse(this.config);
+    const functionToCall = config.getValue("name");
+    // Null handling
+    if (functionToCall == null) {
+      throw new Error("No function name provided");
+    }
+    const functionName = functionToCall.toString();
+    // https://www.assemblyscript.org/runtime.html#memory-layout <
+    if (this.supportedFunctions.includes(functionName) == false) {
+      throw new Error("Function not found");
+    }
     return ["", "functionName"];
   } 
 
   // All Data functions should return two strings, the first being the request payload,
   // and the second being the callback function to pass off the response too.
-  // public SwapData<T>(response: T): [string, string] {
-  //   const config = <JSON.Obj>JSON.parse(this.config);
-  //   // Get the params we know we need
-  //   const poolAddress = config.getString("poolAddress");
-  //   const period = config.getInteger("period");
-  //   // Null handling
-  //   if (poolAddress == null || period == null) {
-  //     throw new Error("Invalid parameters");
-  //   }
-  //   // Send off the response along with the configs, expect a bool and the callback function
-  //   const swapsResult = this.uniswapHandler.getUniswapSwap(response, poolAddress.toString(), i32(period));
+  public SwapData<T>(response: T): Array<string> {
+    const config = <JSON.Obj>JSON.parse(this.config);
+    // Get the params we know we need
+    const poolAddress = config.getString("poolAddress");
+    const period = config.getInteger("period");
+    // Null handling
+    if (poolAddress == null || period == null) {
+      throw new Error("Invalid parameters");
+    }
+    // Send off the response along with the configs, expect a bool and the callback function
+    const swapsResult = this.uniswapHandler.getUniswapSwap(response, poolAddress.toString(), i32(period));
 
-  //   // if swapsResult is false, then recall this, else call the transformer
-  //   if (swapsResult[0] == false) {
-  //     return [swapsResult[1], "SwapData"];
-  //   }
-  //   else {
-  //     // Signal callback to transform
-  //     return ["", "transform"];
-  //   }
+    // if swapsResult is false, then recall this, else call the transformer
+    if (swapsResult[0] != "true") {
+      return [swapsResult[1], "SwapData"];
+    }
+    else {
+      // Signal callback to transform
+      return ["", "transform"];
+    }
     
-  // }
+  }
 
   public transform(): Array<string> {
     return ["", "results"];
   }
 
   // To be called after the main function returns true, to get all data
-  // public results(): string {
-  //   this.TransformedData = this.uniswapHandler.data.toString()
-  //   return this.TransformedData;
-  // }
+  public results(): string {
+    return this.uniswapHandler.data.toString()
+  }
 
   // An array of function to be called, the parameters to be passed, the transformation type, and the transformation parameters
   static exampleInputConfig(): string {
