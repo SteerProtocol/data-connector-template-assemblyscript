a76584bae07f3177ad6924c0859548046a31a86c
Jairus <jairus.v.tanaka@outlook.com>
2023-08-16 09:22:43 -0700

    bug: asyncify freezes build

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,42 +1,38 @@
 //import { Candle, ExecutionContext, DataConnectorConfig, generateCandles, RawTradeData } from "@steerprotocol/strategy-utils/assembly";
 import { JSON } from "json-as/assembly";
-import { fetchSync } from "as-fetch/sync";
+import { fetch } from "as-fetch/assembly";
 import { Config, Swap } from "./types";
-import { SwapParser } from "graph-packet-parser/src/Swap";
 
-let configObj: Config = {
-  "poolAddress": "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8",
-  "lookback": 30000,
-  "candleWidth": "5m",
-  "subgraphEndpoint": "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon",
-  "executionContext": { "epochTimestamp": i32(1691778189730), executionTimestamp: 0, epochLength: 0, vaultAddress: "", blockTime: 0, blockNumber: 0 }
-}
+export { responseHandler } from "as-fetch/assembly";
+
+let configObj: Config = new Config();
 
 export function initialize(config: string): void {
-  //configObj = JSON.parse<Config>(config);
-  //console.log("Received config: " + JSON.stringify(configObj));
+  configObj = JSON.parse<Config>(config);
 }
 
 export function execute(): void {
-  while (true) {
-    const swapData: Swap[] = [];
-    const swapText = fetchSync(configObj.subgraphEndpoint, {
+  //while (true) {
+    //const swapData: Swap[] = [];
+    const swapText = fetch(configObj.subgraphEndpoint, {
       method: "POST",
       mode: "no-cors",
       headers: [],
       body: String.UTF8.encode(
         `{"query":"{ swaps (first: 500, skip: 0, where: {timestamp_gt: ${configObj.executionContext.epochTimestamp - configObj.lookback}, timestamp_lt: ${configObj.executionContext.epochTimestamp}, pool: \\"${configObj.poolAddress.toLowerCase()}\\"}, orderBy: timestamp, orderDirection: asc){id, timestamp, amount0, amount1, transaction {id, blockNumber}, tick, sqrtPriceX96}}"}`
       )
-    }).text();
-    console.log("Response: " + swapText.slice(0, 300) + "...");
+    }).then(res => {
+      console.log("Response: " + res.text())
+    })
+    //console.log("Response: " + swapText.slice(0, 300) + "...");
     // We have iterated through all the swaps.
     // {"data":{"swaps":[]}}
-    if (swapText.length <= 21) return;
+    //if (swapText.length <= 21) break;
 
-    const swaps = changetype<Swap[]>(new SwapParser(swapText).parseToSwaps());
+   /* const swaps = new SwapParser(swapText).parseToSwaps<Swap>();
     console.log(JSON.stringify(swaps));
-    swapData.concat(swaps);
-  }
+    swapData.concat(swaps);*/
+  //}
 }
 
 export function transform(): string {
