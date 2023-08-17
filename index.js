9cc66b44519ad08f13e531b9a6a6713657c09ca2
Jairus <jairus.v.tanaka@outlook.com>
2023-08-17 14:31:03 -0700

    fetch actual data

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { loadWasm } = require("../app-loader");
 loadWasm("./build/debug.wasm", {}).then(mod => {
   mod.initialize(
 `{
-  "poolAddress": "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8",
+  "poolAddress": "0x5645dcb64c059aa11212707fbf4e7f984440a8cf",
   "lookback": 30000,
-  "candleWidth": "5m",
+  "candleWidth": "30m",
   "subgraphEndpoint": "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon",
-  "executionContext": {"epochTimestamp": 1674666008}
+  "executionContext": {"epochTimestamp": 1685678400}
 }`);
+  // Timestamp: 6/1/2023
   console.log("Executing...");
   mod.execute();
   console.log("Finished executing");
